3618a461939ef9f348b5d2a791fba9fd24a0d7d2
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-10 16:36:57 +0200

    modified TestBoxTest

--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -30,6 +31,17 @@ public class TextBoxTest {
         // gender
         $("label[for=gender-radio-1]").click();
 
+        $("input#userNumber.mr-sm-2.form-control").setValue("89999999999");
+
+        // birthday
+//        $("input#dateOfBirthInput.form-control").setSelected(true);
+//        $("input#dateOfBirthInput.form-control").setValue("11 Nov 1972");
+        $("input#dateOfBirthInput.form-control").shouldBe(visible).click();
+        $("select.react-datepicker__month-select").shouldBe(visible).click();
+        $("select.react-datepicker__month-select").selectOption(2);
+       // $("option[value=2]").click();
+
+        $("input#subjectsInput").setValue("Mathematic");
 
         //submit
         $("button#submit.btn.btn-primary").click();
