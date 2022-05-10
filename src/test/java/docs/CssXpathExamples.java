12050ca8d2da0f4521627ef0de4ef35adfe43571
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-10 16:07:01 +0200

    modified TestBoxTest

---
+++ src/test/java/docs/CssXpathExamples.java
@@ -0,0 +1,11 @@
+package docs;
+
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CssXpathExamples {
+    void cssXpathExamples () {
+        //$("[data-testid=email]").
+        //        $(by("data-testid","email"));
+    }
+}
--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -14,23 +14,33 @@ public class TextBoxTest {
     static void beforeAll() {
         System.out.println("beforeAll");
         Configuration.baseUrl="https://demoqa.com";
-        Configuration.browserSize= "1920x1080";
+        Configuration.browserSize= "4320x7680";
     }
 
     @Test
     void successFillTest() {
-        open("/text-box");
-        $(".main-header").shouldHave(text("Text Box"));
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@gmail.com");
-        $("#currentAddress").setValue("Some address");
-        $("#permanentAddress").setValue("Perm address");
-        $("#submit").click();
+        open("/automation-practice-form");
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@gmail.com"));
-        $("#output #currentAddress").shouldHave(text("Some address"));
-        $("#output #permanentAddress").shouldHave(text("Perm address"));
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("input#firstName.mr-sm-2.form-control").setValue("Alex");
+        $("input#lastName.mr-sm-2.form-control").setValue("Petrov");
+        $("input#userEmail.mr-sm-2.form-control").setValue("Petrov@email.com");
+
+        // gender
+        $("label[for=gender-radio-1]").click();
+
+
+        //submit
+        $("button#submit.btn.btn-primary").click();
+
+
+
+        // test after submit
+//        $("#output #name").shouldHave(text("Alex"));
+//        $("#output #email").shouldHave(text("alex@gmail.com"));
+//        $("#output #currentAddress").shouldHave(text("Some address"));
+//        $("#output #permanentAddress").shouldHave(text("Perm address"));
 
 
 
