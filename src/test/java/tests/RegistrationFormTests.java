f31fd7bcef2ee57b350b86cd9ffb2cea86345b5d
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-11 15:10:16 +0200

    modified RegistrationPage

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationFormTests {
-
+RegistrationPage registrationPage = new RegistrationPage();
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -23,32 +23,32 @@ public class RegistrationFormTests {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
 
-        new RegistrationPage().setFirstName("Alex");
-        new RegistrationPage().setLastName("Egorov");
-        new RegistrationPage().setUserEmail("alex@egorov.com");
-        new RegistrationPage().setGender("Other");
-        new RegistrationPage().setNumber("1231231230");
+        registrationPage.setFirstName("Alex");
+        registrationPage.setLastName("Egorov");
+        registrationPage.setUserEmail("alex@egorov.com");
+        registrationPage.setGender("Other");
+        registrationPage.setNumber("1231231230");
 
-        new RegistrationPage().setBirthday("2008", "July", "30");
-        new RegistrationPage().setSubjects("Math");
-        new RegistrationPage().setHobbies("Sports");
-        new RegistrationPage().setPicture("img/1.png");
-        new RegistrationPage().setAddress("Some address 1");
+        registrationPage.setBirthday("2008", "July", "30");
+        registrationPage.setSubjects("Math");
+        registrationPage.setHobbies("Sports");
+        registrationPage.setPicture("img/1.png");
+        registrationPage.setAddress("Some address 1");
 //        new RegistrationPage().setState("NCR");
 //        new RegistrationPage().setCity("Noida");
         $("#submit").pressEnter();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
-        new RegistrationPage().checkForm("Student Name", "Alex Egorov");
-        new RegistrationPage().checkForm("Student Email", "alex@egorov.com");
-        new RegistrationPage().checkForm("Gender", "Other");
-        new RegistrationPage().checkForm("Mobile", "1231231230");
-        new RegistrationPage().checkForm("Date of Birth", "30 July,2008");
-        new RegistrationPage().checkForm("Subjects", "Math");
-        new RegistrationPage().checkForm("Hobbies", "Sports");
-        new RegistrationPage().checkForm("Picture", "1.png");
-        new RegistrationPage().checkForm("Address", "Some address 1");
+        registrationPage.checkForm("Student Name", "Alex Egorov");
+        registrationPage.checkForm("Student Email", "alex@egorov.com");
+        registrationPage.checkForm("Gender", "Other");
+        registrationPage.checkForm("Mobile", "1231231230");
+        registrationPage.checkForm("Date of Birth", "30 July,2008");
+        registrationPage.checkForm("Subjects", "Math");
+        registrationPage.checkForm("Hobbies", "Sports");
+        registrationPage.checkForm("Picture", "1.png");
+        registrationPage.checkForm("Address", "Some address 1");
 //        new RegistrationPage().checkForm("State and City","NCR Noida");
     }
 }
